be97c5b9a116ac0bbb62fbe15d3ec4cd2870d798
KEENNEEDYY <desenvolvedorkennedy@gmail.com>
2024-08-24 16:53:26 -0300

    first test
    Signed-off-by: KEENNEEDYY <desenvolvedorkennedy@gmail.com>

--- src/bottelegram.py
+++ src/bottelegram.py
@@ -5,22 +5,45 @@ api_token = os.getenv('AUTH_TELEGRAM_HTTP_TOKEN')
 
 bot = telebot.TeleBot(api_token)
 
-default_answer = (
-    "Olá seja bem vindo ao atendimento Powerlink Company...\r\n"
-   "Informe o número refente a opção de atendimento desejado:\r"
-   "\n\r\n"
-   "1 - Contratar nossos"
-   " serviços\r\n"
-   "2 - Suporte\r\n"
-   "3 - Realizar pagamento"
-   );
+default_answer = """
+Olá seja bem vindo ao atendimento Powerlink Company...
+Escolha uma opção (CLIQUE NO ITEM): 
+
+/opcao1 - Contratar nossos serviços
+/opcao2 - Suporte
+/opcao3 - Realizar pagamento
+"""
+
+join_service_answer = """
+Informe o CPF do titular do contrato!
+"""
+
+
+@bot.message_handler(commands=["opcao1"])
+def opcao1(user_message):
+    pass
+
+@bot.message_handler(commands=["opcao2"])
+def opcao2(user_message):
+    pass
+
+@bot.message_handler(commands=["opcao3"])
+def opcao3(user_message):
+    bot.send_message(user_message.chat.id, join_service_answer)
+    # CRIAR REGRA DE REGEX PARA PEGAR SOMENTE NUMERO
+
+
+
+
 
 
 def checkMessage(message):
     return True
 
 @bot.message_handler(func=checkMessage)
-def defaultAnswer(question):
-    bot.reply_to(question, default_answer)
+def answer(user_message):
+    bot.reply_to(user_message, default_answer)
+
+
 
 bot.polling()
