18aa184cb9779dbc9bfc5d3ef445257b90f3106e
KEENNEEDYY <desenvolvedorkennedy@gmail.com>
2024-08-27 14:16:59 -0300

    get_customer_details_by_customer_id
    Signed-off-by: KEENNEEDYY <desenvolvedorkennedy@gmail.com>

--- references/testes.py
+++ references/testes.py
@@ -1,5 +1,5 @@
 from payment_api_testes import filtrar_boletos_vencidos, get_customer_details_by_customer_id
-
+import re
 
 boletos_vencidos = filtrar_boletos_vencidos()
 
@@ -7,9 +7,13 @@ for boleto in boletos_vencidos:
     payment_url = boleto.get('payment_url')
     subscription = boleto.get('subscription')
     customer_id = boleto.get('customer_id')
+
+    phone_number_last_9 = (re.sub(r'\D', '', customer_detail.get('phone_number')))[-9:]
+
     customer_detail = get_customer_details_by_customer_id(customer_id)
     print(customer_detail.get('full_name'))
     print(customer_detail.get('phone_number'))
     print(boleto.get('payment_url'))
+    print('\n\n\n\n')
 
 
